d9024ec17530180efe0fef0c38ee467ee784ea81
Marja <castanedamarjaQgmail.com>
2022-01-27 16:23:56 -0600

    Ejercicio para detectar si un numero es par o impar realizado el 27 de Enero del 2022

---
+++ ProyectoPrueba/src/ParImpar.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class ParImpar {
+public static void main (String [] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Escribe un numero: ");
+		
+		int numero1= sc.nextInt();
+		System.out.println(numero1);
+		
+		
+				
+	
+		if ((numero1 % 2) != 0) {
+		//(numero1 % 2 == 0
+			System.out.println ( numero1 + "  No es par");
+		}else {
+			
+			System.out.println (numero1 + "  Es par ");
+		}
+	}
+}
